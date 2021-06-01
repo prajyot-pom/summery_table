4cc7292d7a9c7366f15606c24eacf3c2773a1add
Prajyot Pomannawar <prajyot.pomannawar@afourtech.com>
2021-06-01 11:58:37 +0530

    Added test files

---
+++ main.py
@@ -0,0 +1,51 @@
+import csv
+import os
+import pandas as pd
+import argparse
+
+# field names
+FIELDS = ['Test ID', 'Last Tested', 'Image Version', 'Status', 'Automation', 'Jira', 'Test Description']
+
+# name of csv file
+FILENAME = "university_records.csv"
+FILEPATH = 'test1.md'
+
+
+def create_csv(filename, field):
+    print("Inside create")
+    if not os.path.exists(FILENAME):
+        with open(filename, 'w') as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(field)
+        csvfile.close()
+
+
+def write_data(filename, md_file):
+    with open(filename, 'a+') as csvfile:
+        print("Inside write_data")
+        row = []
+        writer_obj = csv.writer(csvfile)
+        table_data = pd.read_table(md_file, sep="|", header=1, index_col=1, skipinitialspace=True)
+        table_row = table_data.dropna(axis=1, how='all')
+        for i in range(1, 7):
+            var = table_row.iloc[i, -1]
+            if str(var) == 'nan':
+                break
+            var = var.strip()
+            row.append([var])
+        print("before description:: {}".format(row))
+        writer_obj.writerow(row)
+    csvfile.close()
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--file", help="Add md file ", required=True)
+args = parser.parse_args()
+
+
+def main():
+    create_csv(FILENAME, FIELDS)
+    write_data(FILENAME, args.file)
+
+
+main()
---
+++ test1.md
@@ -0,0 +1,30 @@
+# IDT-ACPI-01
+| Key                       | Value                                |
+| -----------               | ------------------------------------ |
+| Test-ID                   | IDT-ACPI-01                          |
+| Last-Tested               | Week                                 |
+| Last-Tested-Image-Version | V1                                   |
+| Status                    | Pass                                 |
+| Automated                 | Yes                                  |
+| Designed-By               | PPP                                  |
+| Keywords                  | ACPI                                 |
+
+## Test Description
+
+Verify a short press of the 'Power' button before staged boot sequence causes a graceful shutdown of the target (~2-5 minutes)
+
+## Dependencies
+
+## Hardware-Requirements
+
+[ ] Need one
+
+## Setup
+
+## Pre-Conditions
+
+## Test Steps
+
+## Expected Outcome
+
+## Comments
